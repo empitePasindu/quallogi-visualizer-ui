538af8b99cb3b07d666ce40d4cf428e07faa06c7
EmpitePasindu <pasindu@empite.com>
2022-12-07 14:15:06 +0530

    minor fixes

--- src/components/BreachedCounterList.tsx
+++ src/components/BreachedCounterList.tsx
@@ -17,8 +17,11 @@ const BreachedCounterItem = (props: { breachCounter: RuleBreachCounter; onClick:
     >
       <div className="row" style={{ backgroundColor: props.breachCounter.selected ? 'lightblue' : '' }}>
         <div className="col-1 bg-border date-column">{props.breachCounter.id}</div>
-        <div className="col-3 bg-border ">{toSimpleDateString(props.breachCounter.startTime)}</div>
-        <div className="col-2 bg-border" style={{ backgroundColor: props.breachCounter.type === ActivityType.rest ? 'lightgreen' : 'lightyellow' }}>
+        <div className="col-4 bg-border d-flex justify-content-around">
+          <p>{toSimpleDateString(props.breachCounter.startTime)}</p>
+          <p>{toSimpleDateString(props.breachCounter.endTime)}</p>
+        </div>
+        <div className="col-1 bg-border" style={{ backgroundColor: props.breachCounter.type === ActivityType.rest ? 'lightgreen' : 'lightyellow' }}>
           {props.breachCounter.type}
         </div>
         <div className="col-6 bg-border">{props.breachCounter.subRule.split('>>')[0]}</div>
